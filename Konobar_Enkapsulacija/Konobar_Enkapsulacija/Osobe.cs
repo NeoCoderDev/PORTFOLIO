82e351f9b19a6784a0694c6e02311787650e0808
NeoCoderDev <n.pejak96@gmail.com>
2024-07-19 18:56:40 +0200

    Encapsulation

---
+++ Konobar_Enkapsulacija/Konobar_Enkapsulacija/Osobe.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domaci_Konobar_Enkapsulacija
+{
+    public class Osobe
+    {
+        private String returnSrba;
+        private String returnMarko;
+        private int brojosoba;
+
+        public String Marko
+        {
+
+            get
+            {
+                return returnMarko;
+            }
+
+            set
+            {
+                returnMarko = value;
+            }
+
+        }
+        public String Srba
+        {
+
+            get
+            {
+                return returnSrba;
+            }
+
+            set
+            {
+                returnSrba = value;
+            }
+
+        }
+        public int broj
+        {
+
+            get
+            {
+                return brojosoba;
+            }
+
+            set
+            {
+                brojosoba = value;
+            }
+
+        }
+
+
+    } 
+}
---
+++ Konobar_Enkapsulacija/Konobar_Enkapsulacija/Program.cs
@@ -0,0 +1,44 @@
+﻿using Domaci_Konobar_Enkapsulacija;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domaci_Enkapsulacija
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ineligible:
+            Osobe osobe = new Osobe();
+
+            osobe.Marko = "Marko Jovanovic";
+            osobe.Srba = "Srba Petrovic";
+            osobe.broj = 10;
+
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("\t KAFANA KOD SRBE PETROVICA");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("----------------------------------------------\n");
+            Console.WriteLine("Molimo vas upisite broj osoba za porudzbinu\n");
+            Console.WriteLine("Unesite broj osoba: ");
+            int brojGostiju = int.Parse(Console.ReadLine());
+            if (brojGostiju >= osobe.broj)
+            {
+                Console.WriteLine("Za {0} osoba je zaduzen iskusni kuvar gazda {1}", brojGostiju, osobe.Srba);
+            }
+            if (brojGostiju < osobe.broj)
+            {
+                Console.WriteLine("Za {0} osoba je zaduzen novi kuvar {1}", brojGostiju, osobe.Marko);
+            }
+            Console.Read();
+        }
+          goto ineligible;  
+        }
+    }
+}
